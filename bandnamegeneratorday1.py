437f4dfdf964816cc5d615a180b91e49c720212b
smresponsibilities <116661430+smresponsibilities@users.noreply.github.com>
2023-11-29 16:55:22 +0530

    one liner

--- bandnamegeneratorday1.py
+++ bandnamegeneratorday1.py
@@ -1,1 +1,4 @@
-print("apke band ka naam " + input("apki city ka naam \n")+" "+input("apke pet ka naam \n"))
+print("Welcome to band name generator with indian stuff");print("apke band ka naam " + input("apki city ka naam ")+input("apke pet ka naam "))
+
+
+#print("apke band ka naam " + input("Welcome to band name generator with indian stuff\napki city ka naam ")+input("apke pet ka naam "))
