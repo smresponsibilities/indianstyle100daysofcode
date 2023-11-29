0dcd7c27fb7ad2e2d42d8fd6a4e9d7755abde8c7
smresponsibilities <116661430+smresponsibilities@users.noreply.github.com>
2023-11-29 23:11:48 +0530

    single liner that works in py3.12

--- tipcalculatorday2.py
+++ tipcalculatorday2.py
@@ -5,3 +5,7 @@ c=input("Kitne aadmi the")
 d=(round((float(a)+(int(b)*float(a))//100)/int(c),2))
 #using aadmi as a gender neutral word
 print(f"Har ek ko itna cash dena hai ₹{d:.2f}")
+
+
+
+#print(f"Har ek ko itna cash dena hai ₹{round(float(input('Welcome to the tip calculator as tip culture is increasing in India\nKitne ka bill hai ₹'))*(1+ float(input('Tip ki percentage? 10, 12, or 15?'))/100 )  / int(input('Kitne aadmi the')), 2):.2f}")
